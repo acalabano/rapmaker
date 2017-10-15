e221b33ce0329ffa460e93344858bd1767174430
Ana Calabano <acalabano@gmail.com>
2017-10-15 14:29:12 -0400

    fixing the CORS

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,13 @@
 var functions = require('firebase-functions')
+const cors = require('cors')({
+  origin: true
+})
 
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  cors((req, res, () => {
+    res.send('Hello from Firebase!')
+  }))
+})
 // // Start writing Firebase Functions
 // // https://firebase.google.com/preview/functions/write-firebase-functions
 //
